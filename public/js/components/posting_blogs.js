9e67f96ad87c88186df76bbb585f392a50514f99
anilpradyumnac <anilpradyumnac@gmail.com>
2015-12-07 20:08:26 +0530

    New fixes merged

--- public/js/components/posting_blogs.js
+++ public/js/components/posting_blogs.js
@@ -4,17 +4,15 @@
 var GetPosts = React.createClass({
 	componentDidMount:function(){
 		$.get('/get_blogs',function(data){
-			alert(data[2].title)
-			for (var i = 0; i < data.length; i++) {
-			            (data[i].title)
-			        }
+			alert(data[2].content)
+			
 		})
 	},
 	render:function(){
 	return(
 		<div >
 				<div id="Title">
-						
+						<p></p>
 				</div>
 		</div>
 	)
