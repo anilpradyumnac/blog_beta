135747d9534700fbb5fc4c2cab2c99709caf8088
Anil Pradyumna Chakicherla <anilpradyumnac@gmail.com>
2015-12-08 20:34:54 +0530

    Update posting_blogs.js

--- public/js/components/posting_blogs.js
+++ public/js/components/posting_blogs.js
@@ -28,39 +28,7 @@ var EachPost = React.createClass({
 var GetPosts = React.createClass({
     
     componentDidMount:function(){
-<<<<<<< HEAD
-        
-        this.loadPage()
-        
-    },
-    loadPage:function(){
-        $.get('/get_blogs',function(data){
-            console.log('data')
-            console.log(data)
-            this.setState({data:data})
-        }.bind(this))
-    },
-    getInitialState:function(){
-        return({
-            data:[{}]
-        })
-    },
-		render:function(){
-		        var content = ''
-		        for(var i=0;i<this.state.data.length;i++){
-		            content += this.state.data[i].title
-		        }
-		        return(
-		        <div >
-		                <div id="Title" >
-		                    <br>{content}</br>
-		                </div>
-		        </div>
-		    )
-		}
-})
-ReactDOM.render(<GetPosts />,document.getElementById('app'))
-=======
+
         this.loadPage()
     },
     loadPage:function(){
